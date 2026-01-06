fe4bb9f9e607195662026fcb9ba0b69f12d4bb72
krishsharma955 <krishsharma9005@gmail.com>
2026-01-06 22:38:04 +0530

    Project Setup

---
+++ src/test/java/com/K955/Blog_App/BlogAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.K955.Blog_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
